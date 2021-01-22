291e21914054578be2b8790a7056bf3e737a72e7
Matt <39469614+KosmicShovel@users.noreply.github.com>
2021-01-22 15:22:05 -0600

    move folder

---
+++ Chapter 3/3.1/prg3-1.c
@@ -0,0 +1,39 @@
+﻿#define io <stdio.h>
+#define i int
+#define r return 0;
+#define v void
+#define a __asm
+#define sys system
+#define p "pause"
+#include io
+
+unsigned char text1[] = "integerVar = %i\n";
+unsigned char text2[] = "floatingVar = %f\n";
+unsigned char text3a[] = "doubleVar = %e\n";
+unsigned char text3b[] = "doubleVar = %g\n";
+unsigned char text4[] = "charVar = %c\n";
+unsigned char text5[] = "boolVar = %i\n";
+
+i main(v) {
+    int       integerVar = 0xFFFFFFFFFFFFFFF8;
+    float     floatingVar = 331.79f;
+    double    doubleVar = 8.44e+11;
+    char      charVar = 0xFFFFFFFFFFFFFFFE;
+    _Bool     boolVar = 0xFFFFFFFFFFFFFFFE;
+
+    a{
+        // Loading 32-bit values
+        mov DWORD PTR integerVar, 0x64
+        // Loading 8-bit values
+        mov BYTE PTR charVar, 87
+        mov BYTE PTR boolVar, 0
+    }
+    printf(text1, integerVar);
+    printf(text2, floatingVar);
+    printf(text3a, doubleVar);
+    printf(text3b, doubleVar);
+    printf(text4, charVar);
+    printf(text5, boolVar);
+    sys(p);
+    r
+}
---
+++ Chapter 3/3.2/prg3-2.c
@@ -0,0 +1,61 @@
+﻿#define io <stdio.h>
+#define i int
+#define r return 0;
+#define v void
+#define asm __asm
+#define sys system
+#define p "pause"
+#include io
+
+unsigned char text1[] = "a - b = %i\n";
+unsigned char text2[] = "b * c = %i\n";
+unsigned char text3[] = "a / c = %i\n";
+unsigned char text4[] = "a + b * c = %i\n";
+unsigned char text5[] = "a * b + c * d = %i\n";
+
+i main(v) {
+    int a = 100, b = 2, c = 25, d = 4;
+    asm{
+        mov eax, a
+        sub eax, b // Subtraction
+        push eax
+        push OFFSET text1
+        call printf
+        add esp, 8
+
+        mov eax, b
+        imul eax, c // Multiplication
+        push eax
+        push OFFSET text2
+        call printf
+        add esp, 8
+
+        mov eax, a
+        cdq
+        idiv c // Division
+        push eax
+        push OFFSET text3
+        call printf
+        add esp, 8
+
+        mov eax, b
+        imul eax, c // Precedence
+        add eax, a
+        push eax
+        push OFFSET text4
+        call printf
+        add esp, 8
+
+        mov eax, a
+        imul eax, b
+        mov ecx, c
+        imul ecx, d
+        add eax,ecx
+        push eax
+        push OFFSET text5
+        call printf
+        add esp, 8
+    }
+    sys(p);
+    r
+}
